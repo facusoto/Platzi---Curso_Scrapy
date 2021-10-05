118400d34cae7119031a39429abe302913fadc97
facusoto <81945511+facusoto@users.noreply.github.com>
2021-10-05 10:57:56 -0300

    Scraper de quotes mejorado, ahora no guarda repetidamente los top ten tags

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,34 +1,57 @@
 import scrapy
 
-# Título = //h1/a/text()
-# Citas = //span[@class="text" and @itemprop="text"]/text()
-# Top ten tags = //span[@class="tag-item"]/a[@class="tag"]/text()
-# next page button = '//ul[@class="pager"]//li[@class="next"]/a/@href'
+"""
+Title = //h1/a/text()
+Quotes = //span[@class="text" and @itemprop="text"]/text()
+Top Ten Tags = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
+Next = //ul[@class="pager"]//li[@class="next"]/a/@href
+"""
+
 
 class QuotesSpider(scrapy.Spider):
-    # name es el nombre unico con el scrapy se va referir al spider dentro del proyect.
-    # name debe ser unico.
-    name = 'quotes'
-    # Defiimos una lista de url a las cuales les vamos a realizar las peticiones http.
+    name = "quotes"
     start_urls = [
-        'http://quotes.toscrape.com/page/1/'
+        "http://quotes.toscrape.com/page/1/"
     ]
     custom_settings = {
-        'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        "FEED_URI": "quotes.json",
+        "FEED_FORMAT": "json"
     }
-    # definir el metodo parse el cual nos sirve para analizar un archivo y extraer informacion valiosa a partir de el.
+
+    def parse_only_quotes(self, response, **kwargs):
+        new_quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
+        kwargs["quotes"].extend(new_quotes)
+
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(
+                next_page_button_link,
+                callback=self.parse_only_quotes,
+                cb_kwargs=kwargs
+            )
+        else:
+            yield kwargs
+
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
-        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//span[@class="tag-item"]/a[@class="tag"]/text()').getall()
 
-        yield {
-            'title': title,
-            'quotes': quotes,
-            'top_ten_tags': top_ten_tags
-        }
+        quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
 
-        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        tags = response.xpath(
+            '//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse)
+            yield response.follow(
+                next_page_button_link,
+                callback=self.parse_only_quotes,
+                cb_kwargs={
+                    "title": title,
+                    "quotes": quotes,
+                    "top_ten_tags": tags,
+                }
+            )
