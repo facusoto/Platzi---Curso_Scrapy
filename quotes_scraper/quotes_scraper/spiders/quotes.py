bd6e8dd9160165f583a6bc7eb748258fc2aa1f12
facusoto <81945511+facusoto@users.noreply.github.com>
2021-11-01 11:03:44 -0300

    Agregada agency, mejoras

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,57 +1,60 @@
 import scrapy
 
-"""
-Title = //h1/a/text()
-Quotes = //span[@class="text" and @itemprop="text"]/text()
-Top Ten Tags = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
-Next = //ul[@class="pager"]//li[@class="next"]/a/@href
-"""
-
-
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        "http://quotes.toscrape.com/page/1/"
-    ]
-    custom_settings = {
-        "FEED_URI": "quotes.json",
-        "FEED_FORMAT": "json"
-    }
+	name = 'quotes'
+	start_urls = [
+		'https://quotes.toscrape.com/'
+	]
 
-    def parse_only_quotes(self, response, **kwargs):
-        new_quotes = response.xpath(
-            '//span[@class="text" and @itemprop="text"]/text()').getall()
-        kwargs["quotes"].extend(new_quotes)
+	custom_settings = {
+		'FEED_URI': 'quotes.json',
+		'FEED_FORMAT': 'json',
+		'CONCURRENT_REQUESTS': 24,
+		'MEMUSAGE_LIMIT_MB': 2048,
+		'MEMUSAGE_NOTIFY_MAIL': ['rvillegas@hoteltec.cl'],
+		'ROBOTSTXT_OBEY': True,
+		'USER_AGENT': 'Rodrigo',
+		'FEED_EXPORT_ENCODING': 'utf8'
+	}
 
-        next_page_button_link = response.xpath(
-            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
-        if next_page_button_link:
-            yield response.follow(
-                next_page_button_link,
-                callback=self.parse_only_quotes,
-                cb_kwargs=kwargs
-            )
-        else:
-            yield kwargs
+	title = '//h1/a/text()'
+	quotes = '//span[@class="text" and @itemprop="text"]/text()'
+	authors = '//small[@class="author" and @itemprop="author"]/text()'
+	top_tags = '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()'
+	next_page_btn = '//ul[@class="pager"]//li[@class="next"]/a/@href'
 
-    def parse(self, response):
-        title = response.xpath('//h1/a/text()').get()
+	def parse_only_quotes(self, response, **kwargs):
+		if kwargs:
+			quotes = kwargs['quotes']
+			authors = kwargs['authors']
+		
+		quotes.extend(response.xpath(self.quotes).getall())
+		authors.extend(response.xpath(self.authors).getall())
 
-        quotes = response.xpath(
-            '//span[@class="text" and @itemprop="text"]/text()').getall()
+		next_page_btn = response.xpath(self.next_page_btn).get()
+		if next_page_btn:
+			yield response.follow(next_page_btn, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
+		else:
+			yield {
+				'quotes':  list(zip(quotes, authors))
+			}
 
-        tags = response.xpath(
-            '//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+	def parse(self, response):		
+		title = response.xpath(self.title).get()
+		quotes = response.xpath(self.quotes).getall()
+		authors = response.xpath(self.authors).getall()
+		top_tags = response.xpath(self.top_tags).getall()
 
-        next_page_button_link = response.xpath(
-            '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
-        if next_page_button_link:
-            yield response.follow(
-                next_page_button_link,
-                callback=self.parse_only_quotes,
-                cb_kwargs={
-                    "title": title,
-                    "quotes": quotes,
-                    "top_ten_tags": tags,
-                }
-            )
+		top = getattr(self, 'top', None)
+		if top:
+			top = int(top)
+			top_tags = top_tags[:top]
+
+		yield {
+			'title': title,
+			'top_tags': top_tags
+		}
+
+		next_page_btn = response.xpath(self.next_page_btn).get()
+		if next_page_btn:
+			yield response.follow(next_page_btn, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes, 'authors': authors})
